869acf42b0d5d713378fb1f1991492b13854de80
SebastianPetak <sebastian.petak@gmail.com>
2017-02-27 15:02:39 -0600

    timestamp.js: Correct basic formatting such as indentation and declaring variables at the start of the document

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -1,27 +1,27 @@
 module.exports = function checkParam(dateParam) {
 	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+	var d;
 	var unixRe = new RegExp('^-?[0-9]+$', 'gm');
 
 	// if parameter was only digits || '-' and digits,
 	// convert seconds (unix time) to milliseconds
 	if (unixRe.test()) {
-		var d = new Date(Math.floor(dateParam * 1000));
+		d = new Date(Math.floor(dateParam * 1000));
 	} else {
-		var d = new Date(dateParam);
+		d = new Date(dateParam);
 	}
 
 	// if d instance of Date is an Invalid Date, return null values,
 	// else return values from d, converting milliseconds to seconds (unix time)
-	if (d == "Invalid Date") {
+	if (d == 'Invalid Date') {
 		return {
-			"unix": null,
-			"natural": null
-		}
+			'unix': null,
+			'natural': null
+		};
 	} else {
 		return {
-			"unix": Math.floor(d.getTime() / 1000),
-			"natural": months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()
-		}
+			'unix': Math.floor(d.getTime() / 1000),
+			'natural': months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()
+		};
 	}
-}
+};
