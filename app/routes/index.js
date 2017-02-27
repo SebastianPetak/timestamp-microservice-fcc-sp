e2e644f4b4d4389d479f6d303184612b31e8d62b
SebastianPetak <sebastian.petak@gmail.com>
2017-02-27 17:31:53 -0600

    timestamp.js is not returning a promise and contains a poorly named variable d. index.js could end in a runtime error if result.unix doesn't exist. Change timestamp.js to return a promise, change the d identifier to dateObj, change the result identifier to be resultDates. Add error handling to the /:date route. Install lodash to check if the result from the api function is empty.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,25 @@ var api = require('./../api/timestamp.js');
 var express = require('express');
 var path = require('path');
 var winston = require('winston');
+var _ = require('lodash');
 
 module.exports = function(app) {
 	app.use(express.static(path.join(process.cwd(), 'public')));
 
 	app.get('/:date', function(req, res) {
 		var dateParam = req.params.date;
-		api(dateParam).then(function(response) {
-			res.status(200).json({
-				unix: response.unix,
-				natural: response.natural
-			});
+		api(dateParam).then(function(result) {
+			if (!_.isEmpty(result) && 'unix' in result && 'natural' in result) {
+				res.status(200).json({
+					unix: result.unix,
+					natural: result.natural
+				});
+			} else {
+				winston.log('warn', 'api function returned an improperly formatted result');
+				res.status(500).json({
+					error: 'Internal Server Error'
+				});
+			}
 		}).catch(function(reason) {
 			winston.log('error', reason);
 			res.status(500).json({
