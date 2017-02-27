3e997146806e18ddbc47df40475a98744b43ff61
SebastianPetak <sebastian.petak@gmail.com>
2017-02-27 14:21:00 -0600

    app/: index.js and timestamp.js contain indentation errors and poorly named parameters. index.html is using linking to an empty css file. Correct indentation errors and rename parameters so their identifiers are more useful and specific. Remove the link to the stylesheet in index.html.

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -1,17 +1,17 @@
-module.exports = function checkParam(p) {
+module.exports = function checkParam(dateParam) {
 	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	
+
 	var unixRe = new RegExp('^-?[0-9]+$', 'gm');
-	
+
 	// if parameter was only digits || '-' and digits,
 	// convert seconds (unix time) to milliseconds
-	if (unixRe.test(p)) {
-		var d = new Date(Math.floor(p * 1000));
+	if (unixRe.test()) {
+		var d = new Date(Math.floor(dateParam * 1000));
 	} else {
-		var d = new Date(p);
+		var d = new Date(dateParam);
 	}
 
-	// if d instance of Date is an Invalid Date, return null values, 
+	// if d instance of Date is an Invalid Date, return null values,
 	// else return values from d, converting milliseconds to seconds (unix time)
 	if (d == "Invalid Date") {
 		return {
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,11 +7,11 @@ module.exports = function(app) {
 
 	app.use(express.static(path.join(process.cwd(), 'public')));
 
-    app.get('/:date', function(req, res) {
-	    res.set({'Content-Type': 'application/json'});
-	    res.status(200);
-	    var param = req.params.date;
-	    var response = api(param);
-	    res.send(JSON.stringify(response));
-    })
+  app.get('/:date', function(req, res) {
+    res.set({'Content-Type': 'application/json'});
+    res.status(200);
+    var param = req.params.date;
+    var response = api(dateParam);
+    res.send(JSON.stringify(response));
+  })
 }
