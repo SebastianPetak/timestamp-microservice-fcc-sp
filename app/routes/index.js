8adf3874aa2c086d71d771e9338bdf94d9273bc3
SebastianPetak <sebastian.petak@gmail.com>
2017-02-27 15:00:11 -0600

    app/: Missing linting and logging tools. index.js should utilize promises when using the timestamp.js module. Install eslint and winston. Modify the /:date route in index.js to use promises.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,33 @@
 var api = require('./../api/timestamp.js');
 var express = require('express');
-var app = express();
 var path = require('path');
+var winston = require('winston');
 
 module.exports = function(app) {
-
 	app.use(express.static(path.join(process.cwd(), 'public')));
 
-  app.get('/:date', function(req, res) {
+	app.get('/:date', function(req, res) {
+		var dateParam = req.params.date;
+		api(dateParam).then(function(response) {
+			res.status(200).json({
+				unix: response.unix,
+				natural: response.natural
+			});
+		}).catch(function(reason) {
+			winston.log('error', reason);
+			res.status(500).json({
+				error: 'Internal Server Error'
+			});
+		});
+	});
+};
+/*
     res.set({'Content-Type': 'application/json'});
     res.status(200);
     var param = req.params.date;
     var response = api(dateParam);
     res.send(JSON.stringify(response));
+
   })
 }
+	*/
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+global.Promise = require('bluebird');
 var port = Number(process.env.PORT || 8000);
 var routes = require('./app/routes/index.js');
 
